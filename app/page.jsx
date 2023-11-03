a74f432608213fc6368f4177d0c2be477bc7a107
Cooper Senior <116678356+coopersenior@users.noreply.github.com>
2023-11-03 14:53:48 -0600

    updates

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import CountButton from "./CountButton/CountButton"
+import SearchBar from "./SearchBar/SearchBar"
+
+export default function Page() {
+  return <div>
+    <CountButton name={"Add One"} mult={1} size={0.5}/>
+    <CountButton name={"Add Three"} mult={3} size={2}/>
+    <hr/>
+    <p style={{textAlign:"center", fontSize:"1.2rem"}}><a href="https://github.com/coopersenior/NextJS">My GitHub Repo</a></p>
+    <hr/>
+    <SearchBar/>
+  </div>
+  
+}
